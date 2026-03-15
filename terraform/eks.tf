77f38684caf8e52755a79e53feac037e9d19e860
Subhash Kumar Singh <skumarsingh@synamedia.com>
2026-03-15 14:01:41 +0530

    fix eks node group

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -9,4 +9,14 @@ module "eks" {
   subnet_ids = module.vpc.private_subnets
 
   enable_irsa = true
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.medium"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+  }
 }
