2c175827afb0515da7e5948b4268d473f8bfd01e
Subhash Kumar Singh <skumarsingh@synamedia.com>
2026-03-16 09:33:20 +0530

    update the instance type

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,11 +37,11 @@ module "eks" {
 
   eks_managed_node_groups = {
     default = {
-      desired_size = 2
+      desired_size = 1
       min_size     = 1
-      max_size     = 3
+      max_size     = 2
 
-      instance_types = ["t3.medium"]
+      instance_types = ["t3.micro"]
 
       capacity_type = "ON_DEMAND"
     }
